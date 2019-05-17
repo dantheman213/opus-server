8ddf8d1f92b7c76e5af040c5eae83511b49e43b6
Daniel Gillespie <daniel.gillespie@pluto.tv>
2019-05-16 22:00:57 -0700

    working on media scanner in background. added some files from master branch as well

--- src/main/java/server/controllers/MediaController.java
+++ src/main/java/server/controllers/MediaController.java
@@ -4,12 +4,21 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import server.tasks.MediaScanner;
 
 @RestController
 public class MediaController {
 
-    @RequestMapping("/")
+    @RequestMapping("/media")
     public String index() {
         return "hello world";
+    }
+
+    @RequestMapping("/media/refresh")
+    public String refresh() {
+        MediaScanner scanner = new MediaScanner();
+        scanner.scan();
+
+        return "ok";
     }
 }
---
+++ src/main/java/server/tasks/MediaScanner.java
@@ -0,0 +1,54 @@
+package server.tasks;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.tika.parser.mp3.Mp3Parser;
+import org.springframework.scheduling.annotation.Async;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Collection;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MediaScanner {
+    @Async
+    public void scan() {
+        Collection files = FileUtils.listFiles(
+                new File("/media"),
+                new String[] { "wav", "mp3", "ogg", "wma", "aif", "aiff", "aifc", "aac", "flac", "alac" },
+                true
+        );
+
+        System.out.println(String.format("Found %d media items.", files.size()));
+
+        files.forEach(file -> {
+            System.out.println(String.format("Processing file %s now...", file.toString()));
+            Metadata metadata = null;
+            try {
+                InputStream input = new FileInputStream(new File(file.toString()));
+                ContentHandler handler = new DefaultHandler();
+                metadata = new Metadata();
+                Parser parser = new Mp3Parser();
+                ParseContext parseCtx = new ParseContext();
+                parser.parse(input, handler, metadata, parseCtx);
+                input.close();
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+
+            if(metadata != null) {
+                // Retrieve the necessary info from metadata
+                // Names - title, xmpDM:artist etc. - mentioned below may differ based
+                System.out.println("----------------------------------------------");
+                System.out.println("Title: " + metadata.get("title"));
+                System.out.println("Artists: " + metadata.get("xmpDM:artist"));
+                System.out.println("Composer : "+metadata.get("xmpDM:composer"));
+                System.out.println("Genre : "+metadata.get("xmpDM:genre"));
+                System.out.println("Album : "+metadata.get("xmpDM:album"));
+            }
+        });
+    }
+}
